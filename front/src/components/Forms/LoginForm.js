4de0ee4df6c9219dfa6a7c516a602395c96c1648
Nonya Johnson III, Esquire <nonya@business.com>
2023-06-13 11:27:28 -0500

    refactor login error messages

--- front/src/components/Forms/LoginForm.js
+++ front/src/components/Forms/LoginForm.js
@@ -7,38 +7,14 @@ import LabeledInputField from '../Fields/LabeledInputField';
 export default function LoginForm({ setToken }) {
 
   const front = FrontendSettings()
-  const [feedbackMsg, setFeedbackMsg] = useState('');
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
   const [emailFieldError, setEmailFieldError] = useState('');
   const [passwordFieldError, setPasswordFieldError] = useState('');
+  const [feedbackMsg, setFeedbackMsg] = useState('');
 
-  // async function loginUser(credentials) {
-  //   const back = BackendSettings()
-  //   const endpoint = back.getBaseUrl() + back.login
-
-  //   return fetch(endpoint, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({
-  //       email: credentials.email,
-  //       password: credentials.password
-  //     })
-  //   })
-  //   .then(response => response.json())
-  //   .then(data => {
-  //     if (data.hasOwnProperty('key')) {
-  //       console.log(data.key)
-  //       setToken(data.key)
-  //       if (window.location.href.includes(front.verifyEmail)) {
-  //         window.location.href = "/";
-  //       }
-  //     }
-  //   })
-  //   .catch(error => {
-  //     console.error('Error:', error)
-  //   })
-  // }
   const loginUser = async (credentials) => {
     const settings = BackendSettings()
     const endpoint = settings.getBaseUrl() + settings.login
@@ -54,7 +30,6 @@ export default function LoginForm({ setToken }) {
         })
       });
 
-      console.log(response)
       if (response.ok) {
         const data = await response.json()
         if (data.hasOwnProperty('key')) {
@@ -68,31 +43,15 @@ export default function LoginForm({ setToken }) {
         const data = await response.json()
         console.log(data)
         
-        let emailError = ""
-        if (data.hasOwnProperty("email")) {
-          for (let error of data.email) {
-            console.log(error)
-            if (typeof error !== undefined) emailError += error
-          }
-        }
-        console.log(emailError)
-        setEmailFieldError(emailError)
-
-        let passwordError = ""
-        if (data.hasOwnProperty("password")) {
-          for (let error of data.password) {
-            if (error !== undefined) passwordError += error
-          }
-        }
-        setPasswordFieldError(passwordError)
-
-        let nonFieldErrors = ""
-        if (data.hasOwnProperty("non_field_errors")) {
-          for (let error of data.non_field_errors) {
-            if (error !== undefined) nonFieldErrors += error
-          }
-        }
-        setFeedbackMsg(nonFieldErrors)
+        const errorMappings = [
+          { key: "email", setter: setEmailFieldError },
+          { key: "password", setter: setPasswordFieldError },
+          { key: "non_field_errors", setter: setFeedbackMsg }
+        ];
+        
+        errorMappings.forEach(({ key, setter }) => {
+          renderErrorMessage(data, key, setter);
+        });
 
       }
     } catch (error) {
@@ -100,8 +59,15 @@ export default function LoginForm({ setToken }) {
     }
   };
 
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const renderErrorMessage = (data, errors_prop, setErrorMsg) => {
+    let errors = ""
+    if (data.hasOwnProperty(errors_prop)) {
+      for (let error of data[errors_prop]) {
+        if (error) errors += error
+      }
+    }
+    setErrorMsg(errors)
+  }
 
   const handleSubmit = async e => {
       e.preventDefault()
