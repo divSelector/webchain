1224e389f0c89a59d43d620b84fdf37bf65a6fda
Nonya Johnson III, Esquire <nonya@business.com>
2023-07-04 14:09:04 -0500

    App.js -> Root.js

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import Root from './Root';
 import './style/index.css'
 
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
 
