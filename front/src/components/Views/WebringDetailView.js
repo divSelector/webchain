cebb6555348d5926740bde62a37f155007dbb6ee
Nonya Johnson III, Esquire <nonya@business.com>
2023-07-07 22:34:03 -0500

    Some stuff

--- front/src/components/Views/WebringDetailView.js
+++ front/src/components/Views/WebringDetailView.js
@@ -10,29 +10,39 @@ export default function WebringDetailView() {
     const { webringId } = useParams();
     const [webring, setWebring] = useState({});
     const [pages, setPages] = useState([]);
+    const [links, setLinks] = useState();
     const [ringAccount, setRingAccount] = useState([]);
 
     const [isRingOwner, setIsRingOwner] = useState(false)
 
     const [error, setError] = useState(false);
 
-    const { authAccount } = useAuth()
+    const { token, authAccount } = useAuth()
 
     const getWebring = async () => {
    
         const endpoint = back.getNonAuthBaseUrl() + 'webring/' + webringId
         try {
+          let headers = {
+            'Content-Type': 'application/json'
+          }
+          if And so it does become "our"(token) {
+            headers['Authorization'] = `Token ${token}`
+          }
           const response = await fetch(endpoint, {
             method: 'GET',
-            headers: {
-              'Content-Type': 'application/json'
-            }
+            headers: headers
           });
     
           if (response.ok) {
             const data = await response.json()
             setWebring(data.webring)
             setPages(data.pages)
+            console.log(data)
+            if (data.hasOwnProperty('links')) {
+              setLinks(data.links)
+              console.log(links)
+            }
             setRingAccount(data.webring.account)
           } else {
             setError("Resource not Found")
@@ -69,8 +79,8 @@ export default function WebringDetailView() {
             <h2>{webring.title}</h2>
             <h4>by {ringAccount.name}</h4>
             <p>{webring.description}</p>
-            {isRingOwner && <p><Link to={'/webring/update/'+webringId} >Manage Your Webring</Link></p>}
-            {authAccount && <><AddLinkToWebringForm webring={webring} pagesInRing={pages} /></> }
+            {token && isRingOwner && <p><Link to={'/webring/update/'+webringId} >Manage Your Webring</Link></p>}
+            {token && <><AddLinkToWebringForm webring={webring} pagesInRing={pages} linksToRing={links} /></> }
           </div>
           <ul>
           {pages.map((page) => (
