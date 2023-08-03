1bc77b4a44e5078b260416ecab9f89e5635e94e3
Nonya Johnson III, Esquire <nonya@business.com>
2023-08-02 21:50:16 -0500

    dfsd

--- front/src/context/CacheContext.js
+++ front/src/context/CacheContext.js
@@ -1,9 +1,21 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect } from 'react';
 
 const CacheContext = createContext();
 
 export function CacheProvider({ children }) {
-  const responseCache = new Map();
+  const storedCache = localStorage.getItem('responseCache');
+  const initialCache = storedCache ? new Map(JSON.parse(storedCache)) : new Map();
+
+  const [responseCache, setResponseCache] = useState(initialCache);
+
+  useEffect(() => {
+    localStorage.setItem(
+      'responseCache', 
+      JSON.stringify(
+        Array.from(responseCache.entries())
+      )
+    );
+  }, [responseCache]);
 
   return (
     <CacheContext.Provider value={responseCache}>
